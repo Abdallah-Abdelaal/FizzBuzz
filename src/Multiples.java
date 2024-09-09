2fac2caea90293c7daa469053d8ce3bc2e6ffcf0
Abdallah-Abdelaal <abdallahabdelaal2005@gmail.com>
2024-09-09 14:39:05 -0400

    coms

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples());
+    }
+
+    public static int multiples() {
+
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean dBy3 = i % 3 == 0;
+            boolean dBy5 = i % 5 == 0;
+
+            if (dBy3 || dBy5) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
